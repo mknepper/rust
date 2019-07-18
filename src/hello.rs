26642a3e9904b3c75b1adf9b4c022a34361f61f6
mknepper <jake.m.knepper@gmail.com>
2019-07-18 12:18:14 -0500

    Tweaks.

--- src/hello.rs
+++ src/hello.rs
@@ -2,6 +2,6 @@
 fn main() {
 // The println! code prints "Hello world!" to our terminal.
     println!("Hello world!");
-// We use a semi1colon to specify the end of the instruction.
+// We use a semicolon to end the instruction following our macro of println!.
 }
 // This final brack closes the function's parameters and ends the main fuction.
