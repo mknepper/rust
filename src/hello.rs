469d51a8f363bdb79b07774f4244503d1a03ae69
mknepper <jake.m.knepper@gmail.com>
2019-07-18 12:16:41 -0500

    Added hello.rs

---
+++ src/hello.rs
@@ -0,0 +1,7 @@
+// Our main function which we'll put our code inside.
+fn main() {
+// The println! code prints "Hello world!" to our terminal.
+    println!("Hello world!");
+// We use a semi1colon to specify the end of the instruction.
+}
+// This final brack closes the function's parameters and ends the main fuction.
