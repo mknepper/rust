a13c8c73f1e47713438dcc9e578940955a4a4915
mknepper <jake.m.knepper@gmail.com>
2020-10-18 03:47:52 -0500

    Directory changes.

---
+++ strings/hello_world.rs
@@ -0,0 +1,7 @@
+// Our main function which we'll put our code inside.
+fn main() {
+// The println! code prints "Hello world!" to our terminal.
+    println!("Hello world!");
+// We use a semicolon to end the instruction following our macro of println!.
+}
+// This final bracket closes the function's parameters and ends the main fuction.
