a85bad8374d7ad9920f42f250cbed784cecb3f2b
mknepper <jake.m.knepper@gmail.com>
2020-11-23 12:03:43 -0600

    Added comments.

--- variables/variables.rs
+++ variables/variables.rs
@@ -1,4 +1,7 @@
+// Our main function.
 fn main() {
+    // "let" declares we're using a variable, "x" being our variable.
     let x = 3;
+    // We set x to equal 3. Below we print x, using "{}" for x's input.
     println!("{}", x)
 }
